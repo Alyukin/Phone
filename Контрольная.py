025218433df785cdd654d80653c40a6fcad60cae
Alyukin <77451834+Alyukin@users.noreply.github.com>
2021-12-04 17:38:22 +0300

    Update Контрольная.py

--- Контрольная.py
+++ Контрольная.py
@@ -1,1 +1,20 @@
-class
+class PhoneContact:
+    def __init__(self, name, contactNumber, note):
+        self.name = name
+        self.contactNumber = contactNumber
+        self.note = note
+
+        def display(self):
+            print(self.name, self.contactNumber, self.note)
+
+
+class PhoneBook:
+    def __init__(self, contactNumber):
+        self.contactNumber = contactNumber
+        self.contactNumber = []
+
+    def getContactByName(self):
+        self.name = print(self.contactNumber)
+    def newContact(self):
+        input(self.name)
+        input(self.contactNumber)
